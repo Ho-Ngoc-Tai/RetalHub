572d56b613ca9a6d80da2f446a1e32a5a684c74a
Tai <hongoctai250402@gmail.com>
2025-11-03 17:57:53 +0700

    first add project

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+  ],
+  providers: [PrismaService],
+})
+export class AppModule {}
